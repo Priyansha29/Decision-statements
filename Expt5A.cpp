926b95f8a8eda13fa5f0595cf19603d1f0eed245
Priyansha29 <priyanshagour29@gmail.com>
2025-07-20 20:12:53 +0530

    Create Expt5A.cpp

---
+++ Expt5A.cpp
@@ -0,0 +1,21 @@
+//PRIYANSHA GOUR 
+//24070123079
+//ENTC-A3
+//EXPERIMENT- 5
+#include <iostream>
+using namespace std;
+int main() {
+    int a, b, c;
+    cout<<"enter a number : ";
+    cin>>a;
+    if(a%2==0){
+        cout<<"NUmber is EVEN"<<endl;
+    }
+    else{
+        cout<<"NUmber is ODD"<<endl;
+    }
+    
+    
+
+    return 0;
+}
