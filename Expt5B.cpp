2b5a6355065fdb39a781a13ec980f1aacd7026e8
Priyansha29 <priyanshagour29@gmail.com>
2025-07-20 20:13:20 +0530

    Create Expt5B.cpp

---
+++ Expt5B.cpp
@@ -0,0 +1,19 @@
+//PRIYANSHA GOUR 
+//24070123079
+//ENTC-A3
+//EXPERIMENT- 5
+#include <iostream>
+using namespace std;
+int main() {
+    char p;
+    cout<<"enter a character : ";
+    cin>>p;
+    if(p=='a' || p=='e' || p=='i' ||  p=='o' ||  p=='u' || p=='A' || p=='E' || p=='I' ||  p=='O' ||  p=='U'){
+        cout<<"THIS IS A VOWEL"<<endl;
+    }
+    else{
+        cout<<"THIS IS A CONSONANT"<<endl;
+    }
+    
+    return 0;
+}
